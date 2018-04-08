221579157b2e35eb17329880379c748e29adb673
CruzJeff <26846343+CruzJeff@users.noreply.github.com>
2018-04-08 08:39:02 -0400

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -47,15 +47,35 @@ CS_Test["Label"] = ["Computer Science" for x in range(len(CS_Test))]
 Eco_Test["Label"] = ["Ecology" for x in range(len(Eco_Test))]
 Bio_Test["Label"] = ["Biology" for x in range(len(Bio_Test))]
 
+#Plot Distribution of Disciplines in Training Set
+labels = ["Chemistry", "Computer Science", "Ecology", "Biology"]
+from IPython.core.pylabtools import figsize
+figsize(12.5,3.5)
+y_pos = np.arange(4)
+plt.style.use('ggplot')
+plt.rcParams['font.size'] = 12
+plt.figure(figsize=(2,10))
+plt.bar(y_pos, [len(Chem_Abstracts), len(CS_Abstracts), len(Eco_Abstracts), len(Bio_Abstracts)])
+plt.xticks(y_pos, labels)
+plt.legend(fontsize=6)
+plt.title('Number of Abstracts per Discipline (Training Data)', fontsize=24)
+plt.show()
+
+#Plot Distribution of Disciplines in Training Set
+plt.style.use('ggplot')
+plt.rcParams['font.size'] = 12
+plt.figure(figsize=(2,10))
+plt.bar(y_pos, [len(Chem_Test), len(CS_Test), len(Eco_Test), len(Bio_Test)])
+plt.xticks(y_pos, labels)
+plt.legend(fontsize=6)
+plt.title('Number of Abstracts per Discipline (Testing Data)', fontsize=24)
+plt.show()
+
 #Creating the Analyze method to calculate features of the Abstracts
 def Analyze(dataframe):
-    
     dataframe["Num_Words"] = dataframe["Abstracts"].apply(lambda x: len(str(x).split()))
-    
     dataframe["num_unique_words"] = dataframe["Abstracts"].apply(lambda x: len(set(str(x).split())))
-
     dataframe["num_stopwords"] = dataframe["Abstracts"].apply(lambda x: len([w for w in str(x).lower().split() if w in eng_stopwords]))
-
     dataframe["num_punctuations"] = dataframe["Abstracts"].apply(lambda x: len([c for c in str(x) if c in string.punctuation]) )
 
 #Doing the Analysis on all of the data
@@ -67,6 +87,60 @@ Analyze(Chem_Test)
 Analyze(Bio_Test)
 Analyze(CS_Test)
 Analyze(Eco_Test)
+
+#Plotting the metrics
+
+Num_Words_Mean = [np.mean(Chem_Abstracts["Num_Words"]),np.mean(CS_Abstracts["Num_Words"]),
+                np.mean(Eco_Abstracts["Num_Words"]),np.mean(Bio_Abstracts["Num_Words"])]
+
+Num_Unique_Words_Mean = [np.mean(Chem_Abstracts["num_unique_words"]),np.mean(CS_Abstracts["num_unique_words"]),
+                np.mean(Eco_Abstracts["num_unique_words"]),np.mean(Bio_Abstracts["num_unique_words"])]
+
+Num_Stopwords_Mean = [np.mean(Chem_Abstracts["num_stopwords"]),np.mean(CS_Abstracts["num_stopwords"]),
+                np.mean(Eco_Abstracts["num_stopwords"]),np.mean(Bio_Abstracts["num_stopwords"])]
+
+Num_Punctuations_Mean = [np.mean(Chem_Abstracts["num_punctuations"]),np.mean(CS_Abstracts["num_punctuations"]),
+                np.mean(Eco_Abstracts["num_punctuations"]),np.mean(Bio_Abstracts["num_punctuations"])]
+
+#Plot Average Number of Words per Abstract
+plt.style.use('ggplot')
+plt.rcParams['font.size'] = 12
+plt.figure(figsize=(2,10))
+plt.bar(y_pos, Num_Words_Mean)
+plt.xticks(y_pos, labels)
+plt.legend(fontsize=6)
+plt.title('Average Number of Words Per Abstract', fontsize=24)
+plt.show()
+
+#Plot Average Number of Unique Words per Abstract
+plt.style.use('ggplot')
+plt.rcParams['font.size'] = 12
+plt.figure(figsize=(2,10))
+plt.bar(y_pos, Num_Unique_Words_Mean)
+plt.xticks(y_pos, labels)
+plt.legend(fontsize=6)
+plt.title('Average Number of Unique Words Per Abstract', fontsize=24)
+plt.show()
+
+#Plot Average Number of Stop Words per Abstract
+plt.style.use('ggplot')
+plt.rcParams['font.size'] = 12
+plt.figure(figsize=(2,10))
+plt.bar(y_pos, Num_Stopwords_Mean)
+plt.xticks(y_pos, labels)
+plt.legend(fontsize=6)
+plt.title('Average Number of StopWords Per Abstract', fontsize=24)
+plt.show()
+
+#Plot Average Number of Punctuations per Abstract
+plt.style.use('ggplot')
+plt.rcParams['font.size'] = 12
+plt.figure(figsize=(2,10))
+plt.bar(y_pos, Num_Punctuations_Mean)
+plt.xticks(y_pos, labels)
+plt.legend(fontsize=6)
+plt.title('Average Number of Punctuations Per Abstract', fontsize=24)
+plt.show()
 
 #Combining all the Training Data
 Train = pd.concat([Eco_Abstracts,CS_Abstracts,Bio_Abstracts, Chem_Abstracts], ignore_index=True)
@@ -81,6 +155,41 @@ Y_Train = Train["Label"]
 #Creating X_Test and Y_Test
 X_Test = Test.iloc[:,2:6]
 Y_Test = Test["Label"]
+
+#Creating First XGBoost Classifier
+boost = xgboost.XGBClassifier(n_estimators=800)
+boost.fit(X_Train,Y_Train)
+
+#Getting predictions
+pred = list(boost.predict(X_Test))
+true = list(Y_Test)
+
+#Checking how many errors there are
+error = 0
+for x in range(len(true)):
+    if true[x] != pred[x]:
+        error += 1
+
+#Creating confusion matrix
+plt.rcParams.update(plt.rcParamsDefault)
+
+y_actu = pd.Series(true, name='Actual')
+y_pred = pd.Series(pred, name='Predicted')
+df_confusion = pd.crosstab(y_actu, y_pred)
+
+def plot_confusion_matrix(df_confusion, title='Confusion matrix', cmap=plt.cm.gray_r):
+    plt.matshow(df_confusion, cmap=cmap) # imshow
+    #plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(df_confusion.columns))
+    plt.xticks(tick_marks, df_confusion.columns, rotation=45)
+    plt.yticks(tick_marks, df_confusion.index)
+    #plt.tight_layout()
+    plt.ylabel(df_confusion.index.name)
+    plt.xlabel(df_confusion.columns.name)
+
+plot_confusion_matrix(df_confusion)
+    
 
 #Finding the 100 most common words across all the abstracts
 words = []
@@ -108,7 +217,7 @@ x_test = pd.DataFrame(tokenize.texts_to_matrix(Test["Abstracts"]))
 X_Train = pd.concat([X_Train,x_train], axis = 1)
 X_Test = pd.concat([X_Test,x_test], axis = 1)
 
-#Creating XGBoost Classifier
+#Creating Second XGBoost Classifier
 boost = xgboost.XGBClassifier(n_estimators=800)
 boost.fit(X_Train,Y_Train)
 
@@ -123,6 +232,8 @@ for x in range(len(true)):
         error += 1
 
 #Creating confusion matrix
+plt.rcParams.update(plt.rcParamsDefault)
+
 y_actu = pd.Series(true, name='Actual')
 y_pred = pd.Series(pred, name='Predicted')
 df_confusion = pd.crosstab(y_actu, y_pred)
